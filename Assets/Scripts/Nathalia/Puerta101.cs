411b0e333133ff9b298555f7eba1785f6332f13c
Isabedo <99741288+Isabedo@users.noreply.github.com>
2026-03-09 13:41:40 -0500

    Nive2_NathaListo

---
+++ Assets/Scripts/Nathalia/Puerta101.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puerta101 : MonoBehaviour
+{
+    [SerializeField] Collider2D puertaCollider;
+    bool puertaAbierta;
+    [SerializeField] Animator animator;
+    [SerializeField] int toquesParaAbrir = 30;
+    int toquesActuales;
+
+    void Start()
+    {
+        puertaCollider = GetComponent<Collider2D>();
+        puertaAbierta = false;
+        toquesActuales = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puertaAbierta)
+        {
+            return;
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (puertaCollider != null && puertaCollider.OverlapPoint(touchPosition))
+                {
+                    toquesActuales++;
+                    Debug.Log("Tocando la puerta. Toques actuales: " + toquesActuales);
+
+                    if (toquesActuales >= toquesParaAbrir)
+                    {
+                        AbrirPuerta();
+                    }
+                }
+            }
+        }
+    }
+
+    void AbrirPuerta()
+    {
+        if (!puertaAbierta)
+        {
+            puertaAbierta = true;
+            Debug.Log("Abriendo la puerta");
+            puertaCollider.enabled = false;
+            if (animator != null)
+            {
+                animator.SetTrigger("abre");
+            }
+            
+        }
+    }
+}
